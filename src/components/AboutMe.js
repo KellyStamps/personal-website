76dfc621a04671e6ca6b3d03e8f820b2464ece9e
KellyStamps <kellydsample@gmail.com>
2018-04-04 19:16:02 -0400

    made design responsive to screensize

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -4,7 +4,7 @@ class AboutMe extends React.Component {
   render(){
     return (
       <div className='about'>
-        <h1>Kelly Sample | <span className='pink-head'> Fullstack Web Developer</span></h1>
+        <h1>Kelly Sample  <span className='pink-head'> Fullstack Web Developer</span></h1>
         <div id='about-para'>
             <p>A NYC based full stack web developer fluent in JavaScript, React, Redux, Ruby, Rails, SQL, PostgreSQL, HTML5, CSS, and GitHub/Git. </p>
             <p>I'm creating something new every week and <a href="https://medium.com/@kellydsample" target="_blank"> blogging</a> about the experience. </p>
@@ -13,7 +13,7 @@ class AboutMe extends React.Component {
         </div>
 
         <div id='about-picture'>
-          <img id='xmas-tree' src={require('../images/XmasTree.jpeg')} height="300"/>
+          <img id='xmas-tree' src={require('../images/XmasTree.jpeg')} height="350"/>
         </div>
 
       </div>
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -8,23 +8,25 @@ class Projects extends React.Component {
 
       <h1>Projects</h1>
 
-      <div className='video-1'>
-        <h2 className="pink-head">Code Challenger</h2>
-        <ReactPlayer className='video' url="https://youtu.be/pus0LMkY-4U" width="510px" height="230px"/>
+      <div className='video-div'>
+        <h2>Code Challenger</h2>
+        <ReactPlayer className='video' url="https://youtu.be/pus0LMkY-4U" width="70%" height="70%"/>
         <p>Challenge yourself to build your coding skills and craft a portfolio page of finished projects.</p>
-        <p className="pink-head">JavaScript| React | Redux | HTML5 | CSS || Rails | PostgreSQL</p>
+        <p className="pink-head">JavaScript| React | Redux | HTML5 | CSS </p>
+        <p className="pink-head"> Rails | PostgreSQL</p>
       </div>
 
-      <div className='video-2'>
-        <h2 className="pink-head">Janx</h2>
-        <ReactPlayer className='video' url="https://youtu.be/6lECuoTIYco" width="510px" height="230px"/>
+      <div className='video-div'>
+        <h2>Janx</h2>
+        <ReactPlayer className='video' url="https://youtu.be/6lECuoTIYco" width="70%" height="70%"/>
         <p>Chat with friends in real time. Log in and find an existing chat, or create your own.</p>
-        <p className="pink-head">JavaScript| React | HTML5 | CSS || Websockets (ActionCable) | Rails | PostgreSQL</p>
+        <p className="pink-head">JavaScript| React | HTML5 | CSS</p>
+        <p className="pink-head">Websockets (ActionCable) | Rails | PostgreSQL</p>
       </div>
 
-      <div className='video-3'>
-        <h2 className="pink-head">Lipstick Finder</h2>
-        <ReactPlayer className='video' url="https://youtu.be/Riu_P4xfWLU" width="510px" height="230px" />
+      <div className='video-div'>
+        <h2>Lipstick Finder</h2>
+        <ReactPlayer className='video' url="https://youtu.be/Riu_P4xfWLU" width="70%" height="70%" />
         <p>Sort through budgest lipstick options by search for key terms or choosing a brand from the drop down menu.</p>
         <p className="pink-head">JavaScript| React | HTML5 | CSS | API </p>
       </div>
