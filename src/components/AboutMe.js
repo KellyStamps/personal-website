b6c610313b37191e608a38f597d07ae93f39526f
KellyStamps <kellydsample@gmail.com>
2018-04-25 15:10:55 -0400

    added new blog

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -9,7 +9,7 @@ class AboutMe extends React.Component {
             <p>A NYC based full stack web developer fluent in JavaScript, React, Redux, Ruby, Rails, SQL, PostgreSQL, HTML5, CSS, and GitHub/Git. </p>
             <p>I'm creating something new every week and <a href="https://medium.com/@kellydsample" target="_blank"> blogging</a> about the experience. </p>
 
-            <p>My current project is <a href="https://github.com/KellyStamps/auth-app" target="_blank">a React + Rails app</a> utilizing authorization and authentication. </p>
+            <p>My current project is <a href="https://medium.com/@kellydsample/challenge-3-run-a-vanilla-js-project-in-your-browser-with-node-791e124aa2c6" target="_blank">learning how to launch a vanilla JS app in the browser with Node.</a></p>
         </div>
 
         <div id='about-picture'>
